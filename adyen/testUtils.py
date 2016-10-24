0df5e4dbfd0feb76b63c8c51658aa78b7b9d4caf
johnd <johnd@localhost>
2016-10-24 16:42:43 -0700

    Improved testUtils

--- adyen/testUtils.py
+++ adyen/testUtils.py
@@ -1,17 +1,28 @@
 
-import datetime
+import datetime, time, json
 
 
 TEST_EUR_AMOUNT = {"value": 1500, "currency" : "EUR"}
  
-TEST_CARD_VISA = \
-      {                                      
-      "number": "4111111111111111",                 
-      "expiryMonth": "8",                           
-      "expiryYear": "2018",                         
-      "cvc": "737",                                 
-      "holderName": "John Smith"                    
+TEST_CARD_VISA = {                                      
+    "number": "4111111111111111",                 
+    "expiryMonth": "8",                           
+    "expiryYear": "2018",                         
+    "cvc": "737",                                 
+    "holderName": "John Smith"                    
+  }
+
+
+MP_TEST_PERSON = {
+  "email":"test@adyen.com",
+  "individualDetails": {
+    "name": {
+      "firstName":"First name",
+      "gender":"MALE",
+      "lastName":"TestData"
       }
+    } 
+  }
 
 
 class CardPayment(object):
@@ -26,3 +37,12 @@ class CardPayment(object):
 
 def timestampMerchantRef():
   return "Test @ " + str(datetime.datetime.now())
+
+def uniquenameFromTimestamp():
+  return int( time.mktime(datetime.datetime.now().timetuple()) ).__str__()
+  
+def jsonFromFile(jsonFileName):
+  with open(jsonFileName) as jsonFile:    
+    data = json.load(jsonFile)
+
+  return data
